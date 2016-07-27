8f8e838cd3584bbad661d4cee0144566bd1445ab
ketanbgm <ketanbgm@gmail.com>
2016-07-27 17:00:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function w3_open() {
+  document.getElementById("mySidenav").style.display = "block";
+}
+function w3_close() {
+  document.getElementById("mySidenav").style.display = "none";
+}
+function openLeftMenu() {
+    document.getElementById("leftMenu").style.display = "block";
+}
+function closeRightMenu() {
+    document.getElementById("leftMenu").style.display = "none";
+}
